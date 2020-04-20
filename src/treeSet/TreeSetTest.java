71d6a0acd64cbd13d0d8b8820d9cf3417d184683
YHJ <765815153@qq.com>
2020-04-20 15:25:24 +0800

    TreeSet

--- src/treeSet/TreeSetTest.java
+++ src/treeSet/TreeSetTest.java
@@ -1,4 +1,19 @@
 package treeSet;
 
+import java.util.*;
+
 public class TreeSetTest {
+    public static void main(String[] args)
+    {
+        TreeSet<Item> parts = new TreeSet<Item>();
+        parts.add(new Item("Toaster", 1234));
+        parts.add(new Item("Widget", 4562));
+        parts.add(new Item("Modem", 9912));
+        System.out.println(parts);
+
+        TreeSet<Item> sortByDescription = new TreeSet<Item>(Comparator.comparing(Item::getDescription));
+
+        sortByDescription.addAll(parts);
+        System.out.println(sortByDescription);
+    }
 }
